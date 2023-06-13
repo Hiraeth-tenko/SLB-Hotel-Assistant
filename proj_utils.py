884d54a573ed962c20036431c2e771212f139c62
Hiraeth-tenko <18950365798@163.com>
2023-06-13 15:30:57 +0800

    实现了播报当前天气的功能

--- proj_utils.py
+++ proj_utils.py
@@ -1,3 +1,6 @@
 TTS_WAV_FILEPATH = "assert/wav/tts.wav"
 RHASSPY_RECV_WAV_FILEPATH = "rhasspy.wav"
 RHASSPY_RECV_PCM_FILEPATH = "rhasspy.pcm"
+
+WEATHER_CSV_FILEPATH = "assert/weather/citycode.csv"
+WEATHER_TTS_WAV_FILEPATH = "assert/weather/tts.wav"
