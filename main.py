7ec291e4f06a4e23bf2b09e96c16a1351df2108a
Qingzhujiu <Qingzhujiu@qq.com>
2023-06-15 15:15:29 +0800

    主程序更新

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import threading
 import wave
+
+from components.music import play_music
 from components.wav_to_pcm import wav2pcm
 from components.SR import Sr
 from components.TTS import Tts
 from flask import Flask, request
 import re
 import proj_utils
-from program import func_wine_introduce
+from program import func_wine_introduce, func_hint, func_receptionist
 from program import func_weather_reporter
 from program import func_ertertianment_introduce
 import subprocess
@@ -50,39 +52,64 @@ def tts(text):
 
 
 def dispatch(order):
-    pattern = [0]*10
+    patterns = []
     # 识别句子中，是否同时出现了’查询‘和’天气‘
-    pattern[0] = r'(?=.*查询)(?=.*天气)'
+    patterns.append(r'(?=.*查询)(?=.*天气)')
 
     # 识别句子中，是否出现了‘介绍’
-    pattern[1] = r'介绍(.+)'
+    patterns.append(r'介绍(.+)')
 
     # 识别句子中的时间，例如：；请在八点四十七叫醒我’，提取出句中的’八点四十七‘
-    pattern[2] = r"请在(\S+)叫醒我"
+    patterns.append(r"请在(\S+)叫醒我")
 
+    # 识别句子中，是否出现了‘指令’
+    patterns.append("指令")
+    # 识别句子中，是否出现了‘娱乐设施’或‘酒水’或‘呼唤人工’
+
+    patterns.append(r"(娱乐设施|酒水|呼唤人工)")
+
+    # 识别句子中，是否出现了‘播放音乐’
+    patterns.append("播放音乐")
     # 查询天气
-    if re.match(pattern[0], order):
+    if re.match(patterns[0], order):
         func_weather_reporter(order)
         return True
-    # 介绍酒水
-    elif re.match(pattern[1], order):
+
+    # 介绍娱乐设施或酒水
+    elif re.match(patterns[1], order):
         # 介绍酒水
         if func_wine_introduce(order):
-            return True
-        # 介绍娱乐设施
-        elif func_ertertianment_introduce(order):
+            play_wav_file(proj_utils.WINE_TTS_WAV_FILEPATH)
             return True
 
-    elif re.match(pattern[2], order):
+        # 介绍娱乐设施
+        elif func_ertertianment_introduce(order):
+            play_wav_file(proj_utils.ENTERTAINMENT_TTS_WAV_FILEPATH)
+            return True
+
+    elif re.match(patterns[2], order):
         # 在句子中查找匹配
-        match = re.search(pattern[2], order)
+        match = re.search(patterns[2], order)
         time = match.group(1)  # 获取匹配到的时间
         # 创建子线程
         thread = threading.Thread(target=thread_function(time))
         # 启动子线程
         thread.start()
 
+    elif re.match(patterns[3], order):
+        func_hint()
+        play_wav_file(proj_utils.RECE_TTS_WAV_FILEPATH)
         return True
+
+    elif re.match(patterns[4], order):
+        func_receptionist(order)
+        play_wav_file(proj_utils.RECE_TTS_WAV_FILEPATH)
+        return True
+
+    elif re.match(patterns[5], order):
+        play_music(order)
+        return True
+
     else:
         sentence = "抱歉，我没有听清楚您说的话,能否再说一遍？"
         Tts('start text to speech', file=proj_utils.TTS_WAV_FILEPATH).start(sentence)
